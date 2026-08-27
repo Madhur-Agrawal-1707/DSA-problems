0e6654ae55ca5248160f8090952372761fa625d8
Madhur Agrawal <agrawalmadhur699@gmail.com>
2026-08-27 16:55:35 +0530

    Time: 1 ms (99.93%), Space: 46.2 MB (82.9%) - LeetHub

---
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        int[][] rows = new int[9][9];
+        int[][] cols = new int[9][9];
+        int[][] boxs = new int[9][9];
+
+        for(int r=0; r < 9; r++){
+            for(int c = 0; c< 9; c++){
+                if(board[r][c] == '.'){
+                    continue;
+                }
+                
+                
+                int val = board[r][c] - '1';
+                
+                if(rows[r][val] == 1){
+                    return false;
+                }
+
+                rows[r][val] = 1;
+
+                if(cols[c][val] == 1){
+                    return false;
+                }
+
+                cols[c][val] = 1;
+
+                int boxIdx = 3 * (r/3) + (c/3);
+                if(boxs[boxIdx][val] == 1){
+                    return false;
+                }
+
+                boxs[boxIdx][val] = 1;
+            }
+        }
+
+        return true;
+    }
+}
